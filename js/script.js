27c8eed6aad376f46ad907ac4bfc13596aeaba21
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-05 16:32:31 +0100

    refactoring complete

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
+// chiedo nome
+    let userName = prompt("Dimmi il tuo nome");
+    document.getElementById("Benvenuto").innerHTML = "Benvenuto/a " + userName;
+
 // chiedo età
     let ageUser = parseInt(prompt("Scrivi la tua età"));
+    document.getElementById("età").innerHTML = "Hai " + ageUser + " anni";
     console.log (ageUser)
 // chiedo km
     let lengthTravel = parseInt(prompt("Scrivi la lunghezza del tuo viaggio"));
+    document.getElementById("lunghezza").innerHTML = "Andrai a percorrere " + lengthTravel + "km";
     console.log (lengthTravel)
 // definisco prezzo km
     let priceOnlykm = 0.21;
 
 // calcolo prezzo
     let totalPrice = (priceOnlykm * lengthTravel)
+    document.getElementById("costo").innerHTML = "Prezzo finale " + totalPrice + " euro"; 
     console.log (totalPrice)
 
 // calcolo sconto 
